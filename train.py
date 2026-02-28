1b5fa1cb564098c38593a40bd375f17d97c7a6ce
luigibruzzese <luigi.bruzzese@mail.polimi.it>
2026-02-28 16:37:20 +0100

    doubled cuda

--- train.py
+++ train.py
@@ -101,7 +101,7 @@ def train():
     net = ssd_net
 
     if args.cuda:
-        net = ssd_net # torch.nn.DataParallel(ssd_net, device_ids=[0,1])
+        net = torch.nn.DataParallel(ssd_net, device_ids=[0,1])
         cudnn.benchmark = True
 
     if args.resume:
